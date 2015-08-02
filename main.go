10c6a8dd1c26df2b60fecc93cf098db0686649a0
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-08-02 11:31:11 +0700

    handler: add initial concept of HandleReceive

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -13,18 +15,30 @@ var (
 	PORT = flag.String("port", "8000", "listen on specified port")
 )
 
+// HandleIndex handles incoming request to "/"
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello")
+}
+
+// HandleReceive handles incoming POST request to "/receive". Each incoming
+// email handled by custom logic based on request parameter.
+func HandleReceive(w http.ResponseWriter, r *http.Request) {
+	rbody := bufio.NewReader(r.Body)
+	_, err := rbody.WriteTo(os.Stdin)
+	if err != nil {
+		http.Error(w, "failed to write request body", http.StatusInternalServerError)
+	}
+	fmt.Fprintf(os.Stdin, "%s", "\n")
 }
 
 func main() {
 	flag.Parse()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", HandleIndex)
+	r.HandleFunc("/", HandleIndex).Methods("GET")
+	r.HandleFunc("/receive", HandleReceive).Methods("POST")
 
 	http.Handle("/", r)
 	log.Printf("start listening on :%s\n", *PORT)
 	log.Fatal(http.ListenAndServe(":"+*PORT, nil))
-
 }
